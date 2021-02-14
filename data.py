7fa465c54b86dd446d28d19a00db391b3279ead9
devkiratu <dev.kiratu@gmail.com>
2021-02-14 23:32:26 +0300

    add basic app setup

---
+++ data.py
@@ -0,0 +1,25 @@
+def Articles():
+  articles = [
+    {
+      'id': 1,
+      'title': 'Article One',
+      'body': 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorem eius quo ab	praesentium pariatur voluptatum nam cumque alias laudantium reprehenderit, saepequasest.Adipisciabvero, ossimusoditenimillo',
+      'author': 'Rattuz',
+      'created': '14-02-2021'
+    },
+       {
+      'id': 2,
+      'title': 'Article Two',
+      'body': 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorem eius quo ab	praesentium pariatur voluptatum nam cumque alias laudantium reprehenderit, saepequasest.Adipisciabvero, ossimusoditenimillo',
+      'author': 'Sophy',
+      'created': '14-02-2021'
+    },
+       {
+      'id': 3,
+      'title': 'Article Three',
+      'body': 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorem eius quo ab	praesentium pariatur voluptatum nam cumque alias laudantium reprehenderit, saepequasest.Adipisciabvero, ossimusoditenimillo',
+      'author': 'Mgeni',
+      'created': '14-02-2021'
+    }
+  ]
+  return articles
